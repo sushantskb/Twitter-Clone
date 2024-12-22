deb6c1420574bc1ea633f564db4c8cbb99567f1a
sushantskb <sushantsrk2001@gmail.com>
2024-12-22 12:45:26 +0700

    added the header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <>
-      <h1 className="underline text-white text-3xl">Hello World</h1>
+      <Header label="Home" />
     </>
   );
 }
