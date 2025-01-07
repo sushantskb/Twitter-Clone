9f7ab24cde64a53700a7e2d170c45d95d43c792c
sushantskb <sushantsrk2001@gmail.com>
2025-01-07 20:37:17 +0700

    added the post feed in my profile page

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 import UserBio from "@/components/Users/UserBio";
 import UserHero from "@/components/Users/UserHero";
 import useUser from "@/hooks/useUser";
@@ -9,22 +10,23 @@ import { ClipLoader } from "react-spinners";
 const UserView = () => {
   const router = useRouter();
   const { userId } = router.query;
-  
+
   const { data: fetchUser, isLoading } = useUser(userId as string);
   console.log(fetchUser);
-  
-  
 
-  if(isLoading || !fetchUser) {
-    return <div className="flex justify-center items-center h-full">
+  if (isLoading || !fetchUser) {
+    return (
+      <div className="flex justify-center items-center h-full">
         <ClipLoader color="lightblue" size={80} />
-    </div>
+      </div>
+    );
   }
   return (
     <>
       <Header label={fetchUser?.user?.name} showBackArrow />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
     </>
   );
 };
