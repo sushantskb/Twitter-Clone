3e75a3551a2d361f50725c3e19d1f6f1ae4f9fe7
sushantskb <sushantsrk2001@gmail.com>
2025-01-10 13:33:27 +0700

    added the use Follow hook

---
+++ hooks/useFollow.ts
@@ -0,0 +1,57 @@
+import { useCallback, useMemo } from "react";
+import useCurrentUser from "./useCurrentUser";
+import useLoginModal from "./useLoginModal";
+import useUser from "./useUser";
+import axios from "axios";
+import { mutate } from "swr";
+import toast from "react-hot-toast";
+
+const useFollow = (userId: string) => {
+  const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(userId);
+
+  const loginModal = useLoginModal();
+
+  const isFollowing = useMemo(() => {
+    const list = currentUser?.followingIds || [];
+    return list.includes(userId);
+  }, [userId, currentUser?.followingIds]);
+
+  const toggleFollow = useCallback(async () => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    try {
+      let request;
+      if (isFollowing) {
+        request = () => axios.delete("/api/follow", { data: { userId } });
+      } else {
+        request = () => axios.post("/api/follow", { userId });
+      }
+
+      await request();
+
+      mutateCurrentUser();
+      mutateFetchedUser();
+
+      toast.success("Success");
+    } catch (error) {
+      toast.error("Something Went Wrong");
+    }
+  }, [
+    currentUser,
+    isFollowing,
+    userId,
+    mutateCurrentUser,
+    mutateFetchedUser,
+    loginModal,
+  ]);
+
+  return {
+    isFollowing,
+    toggleFollow,
+  };
+};
+
+export default useFollow;
