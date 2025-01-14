2e3c50e7ee67fd990141808d93a8c1c2e2edca2f
sushantskb <sushantsrk2001@gmail.com>
2025-01-14 23:56:45 +0530

    added tje get post hook

---
+++ hooks/usePost.ts
@@ -0,0 +1,17 @@
+import fetcher from "@/lib/fetcher";
+import useSWR from "swr";
+
+const usePost = (postId: string) => {
+  const url = `/api/posts?userId=${postId}`;
+
+  const { data, error, isLoading, mutate } = useSWR(url, fetcher);
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
+
+export default usePost;
