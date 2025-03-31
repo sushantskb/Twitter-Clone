87ffa0aa69907f83448d5c3ae2a6453fe8f05c3f
sushantskb <sushantsrk2001@gmail.com>
2025-03-31 18:33:27 +0530

    production fixes 3

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -41,7 +41,7 @@ export default NextAuth({
       },
     }),
   ],
-  debug: process.env.NODE_ENV === "development",
+  debug: process.env.NODE_ENV === "production",
   session: {
     strategy: "jwt",
   },
